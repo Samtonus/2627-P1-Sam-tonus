d0404232818501b489c6f12c946d6efe41ef7bb0
Samtonus <samtonus3@gmail.com>
2026-09-10 12:37:51 +0200

    week 1 opdracht
    mario af

---
+++ Week 1/mondriaan/sketch.js
@@ -0,0 +1,62 @@
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(220);
+strokeWeight(4);
+// horizontaal
+rect(0, 0, 600, 400);
+fill ('blue');
+rect(70, 0, 200, 50);
+fill ('red');
+rect(0, 0, 70, 50);
+fill ('yellow');
+rect(270, 0, 150, 50);
+fill ('black');
+rect(400, 0, 200, 50);
+
+
+// verticaal
+
+rect(0, 50, 40, 200);
+fill ('yellow');
+rect(40, 200, 40, 150);
+fill ('black');
+rect(160, 50, 200, 80);
+rect(350, 50, 40, 80); 
+fill ('red');
+rect(400, 250, 40, 150);
+fill ('blue');
+rect(390, 50, 210, 150);
+
+rect(530, 250, 70, 110);
+fill ('black');
+rect(200, 200, 100, 100);
+fill ('red');
+rect(300, 130, 90, 130);
+fill ('yellow');
+rect(150, 130, 150,70);
+
+
+// horizontaal
+fill ('red');
+rect(0, 250, 40, 50);
+fill ('blue');
+rect(80, 250, 120, 50);
+fill ('');
+rect(300, 250, 100, 50);
+fill ('white');
+rect(40, 50, 120, 150);
+fill ('yellow');
+rect(400, 250, 130, 150);
+fill ('red');
+rect(80, 300, 100, 100);
+fill ('white');
+rect(00, 340, 80, 60);
+
+
+
+
+}
+
